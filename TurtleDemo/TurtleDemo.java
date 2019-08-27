e6028c61cf276cf3911702591c38e9769bd13e7b
Matt Callaghan <mcallaghan@naperville203.org>
2019-08-26 21:46:26 -0500

    started ch 2 notes through primitives

--- TurtleDemo/TurtleDemo.java
+++ TurtleDemo/TurtleDemo.java
@@ -10,5 +10,34 @@ public class TurtleDemo
         crush.penDown();
         crush.forward(50);        
     }
+    
+    public static void chapter2Notes()
+    {
+        /*
+         * Variables store values to be used later.
+         *      Variables have a type (e.g. int), a name (e.g. width), and a value (e.g. 20).
+         */  
+        
+        int width;   //declaring a variable
+        width = 20;  //assigning a variable
+        
+        /*
+         * Java has several primitive data types:
+         *      boolean holds a value of true or false
+         *      int holds an integer number
+         *      double holds a real number (floating point)
+         *      char holds a single character
+         */
+        
+        // here is a boolean
+        boolean isSummer = true;
+        
+        // here is a double
+        double taxRate = 0.075;
+        
+        // here is a char
+        char letter = 'c';
+    
+    }
 
 }
