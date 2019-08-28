bcf3d4e2cd458426db727877659f7a6e32164ad0
mattcallaghan <mcallaghan@naperville203.org>
2019-08-28 15:05:56 -0500

    finished ch 2 notes

--- TurtleDemo/TurtleDemo.java
+++ TurtleDemo/TurtleDemo.java
@@ -37,7 +37,66 @@ public class TurtleDemo
         
         // here is a char
         char letter = 'c';
-    
+        
+        /*
+         * Objects are entities in a program.
+         *      Objects have properties (attributes)
+         *      Objects are manipulated by invoking methods.
+         *      
+         * ocean and crush reference objects.
+         * 
+         * Classes describe a collection of objects.
+         *      All objects of a class have the same behavior (methods),
+         *      and they have the same types of properties (but may have different values).
+         *      
+         * World and Turtle are classes.
+         */
+        
+        World ocean = new World();
+        
+        
+        /*
+         * We use the new operator to construct (create, instantiate) an object.
+         *   The class of teh object is specified immediately after teh new operator (e.g. Turtle)
+         *   If we have to pass additional information to create the object, arguments are specified
+         *   in parentheses after the class (e.g. ocean)
+         *   A reference to the newly created object is returned by the new operator.
+         */
+        
+        Turtle crush = new Turtle(ocean);
+        
+        /*
+         * When invoking methods, we use the dot operator to invoke a method on an object.
+         *   Some methods take no arguments, but we still need parentheses. (e.g. penDown)
+         *   Some methods take one or more arguments (e.g. forward)
+         *   
+         *   Mutator methods modify the state (i.e. properties/attributes) of the object.
+         *      penDown, forward, setPenColor are all mutator methods.
+         */
+        
+        crush.penDown();
+        crush.forward(50);
+        crush.setPenColor(Color.RED);
+        
+        /*
+         * Accessor Methods return a property of the object. 
+         *   The state of the object is not changed.
+         *   
+         *   getPenWidth is an accessor method.
+         */
+        
+        int penWidth = crush.getPenWidth();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
 }
